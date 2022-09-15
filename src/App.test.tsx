65a1e9dc6791840a9c0b80838216811f63e526de
sajadmsNew <smousavi.ee@gmail.com>
2022-09-15 22:35:13 +0300

    feat-add-graphql

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import {Index} from './index';
+import "@testing-library/jest-dom";
+// import { render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import App from "./App";
+import { GET_COUNTRIES, SEARCH_COUNTRY } from "./utils/Queries";
+import userEvent from '@testing-library/user-event';
+
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Index/>);
+  const linkElement = screen.getByText(/App powered by React/g);
+
   expect(linkElement).toBeInTheDocument();
+});
+
+test('state data',()=>{
+
+//  render(<App/>);
+
+// expect(app.state().shwonData).toEqual([])
+
+
+})
+
+const mocks:any = [
+  {
+    request: {
+      query: GET_COUNTRIES,
+    },
+
+    // error: new Error("An error occurred")
+    // ,
+    result: {
+      data: {
+        countries: [{__typename: "Country", code: "AD",name: "Andorra", phone: "376"}]
+      }
+    }
+  }
+]; // We'll fill this in next
+
+
+it("renders without error", async () => {
+   
+  render(
+    <MockedProvider mocks={mocks} addTypename={true}>
+      <App/>
+    </MockedProvider>
+
+  );
+
+  // expect(await screen.findByText("Loading...")).toBeInTheDocument();
+  // expect(await screen.findByText("there ia issue with internet for all")).toBeInTheDocument();
+
+
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './assets/index.css'
+import { ApolloClient, ApolloLink, InMemoryCache ,ApolloProvider , HttpLink} from "@apollo/client";
+
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+const httpLink = new HttpLink({
+
+  uri: "https://countries.trevorblades.com/",
+
+});
+
+
+
+export const client = new ApolloClient({
+
+  cache: new InMemoryCache(),
+
+  link: ApolloLink.from([errorLink,httpLink]),
+
+});
+
+
+export const Index =()=>{
+  return (<>
+  <ApolloProvider client={client}>
+      <App />
+</ApolloProvider>
+</>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement || document.createElement('div')).render(
+  <Index/>
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
